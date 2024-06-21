d0a06223b75079ca31d9c999703fa7aad1cf4b07
ahmedovelshan <80003705+ahmedovelshan@users.noreply.github.com>
2024-06-21 15:46:51 +0400

    Create provider.tf

---
+++ EKS/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "demo-devops-project"
+    region         = "eu-central-1"
+    key            = "Devops-Demo-Project/terraform.tfstate"
+    dynamodb_table = "terraform-tfstate"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
