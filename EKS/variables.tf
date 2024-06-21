b7f041a6ab1ec355796bb7f90ed68fd948ecba79
ahmedovelshan <80003705+ahmedovelshan@users.noreply.github.com>
2024-06-21 15:54:36 +0400

    Update variables.tf

--- EKS/variables.tf
+++ EKS/variables.tf
@@ -1,71 +1,71 @@
 variable "vpc_name" {
   description = "Name of the VPC"
   type        = string
-  default     = "default-vpc"
+  default     = "devops-vpc"
 }
 
 variable "igw_name" {
   description = "Name of the Internet Gateway"
   type        = string
-  default     = "default-igw"
-}
-
-variable "rt_name2" {
-  description = "Name of the Route Table"
-  type        = string
-  default     = "default-rt2"
+  default     = "devops-igw"
 }
 
 variable "subnet_name" {
   description = "Name of the Subnet"
   type        = string
-  default     = "default-subnet"
+  default     = "devops-subnet"
 }
 
 variable "subnet_name2" {
   description = "Name of the second Subnet"
   type        = string
-  default     = "default-subnet2"
+  default     = "devops-subnet2"
 }
 
 variable "security_group_name" {
   description = "Name of the Security Group"
   type        = string
-  default     = "default-sg"
+  default     = "devops-sg"
+}
+
+variable "rt_name2" {
+  description = "Name of the Route Table"
+  type        = string
+  default     = "devops-route-table2"
 }
 
 variable "iam_role_eks" {
   description = "Name of the IAM Role for EKS"
   type        = string
-  default     = "default-iam-role-eks"
+  default     = "devops-iam-role-eks"
 }
 
 variable "iam_role_node" {
   description = "Name of the IAM Role for EKS Nodes"
   type        = string
-  default     = "default-iam-role-node"
+  default     = "devops-iam-role-ec2"
 }
 
 variable "iam_policy_eks" {
   description = "Name of the IAM Policy for EKS"
   type        = string
-  default     = "default-iam-policy-eks"
+  default     = "devops-iam-policy-eks"
 }
 
 variable "iam_policy_node" {
   description = "Name of the IAM Policy for EKS Nodes"
   type        = string
-  default     = "default-iam-policy-node"
+  default     = "devops-iam-policy-node"
 }
 
 variable "cluster_name" {
   description = "Name of the EKS Cluster"
   type        = string
-  default     = "default-cluster"
+  default     = "devops-EKS-Cluster"
 }
 
 variable "eksnode_group_name" {
   description = "Name of the EKS Node Group"
   type        = string
-  default     = "default-node-group"
+  default     = "devops-Node-Group"
 }
