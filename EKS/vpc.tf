4901fb89dcdaa22d772d9623160e12af72e3c015
ahmedovelshan <80003705+ahmedovelshan@users.noreply.github.com>
2024-06-21 17:20:29 +0400

    Update vpc.tf

--- EKS/vpc.tf
+++ EKS/vpc.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "public-subnet" {
 }
 
 resource "aws_subnet" "public-subnet2" {
-  vpc_id                  = data.aws_vpc.vpc.id
+  vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "eu-central-1b"
   map_public_ip_on_launch = true
@@ -49,10 +49,10 @@ resource "aws_route_table" "rt" {
 }
 
 resource "aws_route_table" "rt2" {
-  vpc_id = data.aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = data.aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
