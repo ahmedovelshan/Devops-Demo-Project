a840ac1f2beb5b1780f3874e5913575ea8440fae
ahmedovelshan <80003705+ahmedovelshan@users.noreply.github.com>
2024-06-30 18:08:16 +0400

    Update provider.tf

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "demo-devops-project"
     region         = "eu-central-1"
     key            = "Demo-Devops-Project/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "terraform-tfstate"
     encrypt        = true
   }
   required_version = ">=0.13.0"
